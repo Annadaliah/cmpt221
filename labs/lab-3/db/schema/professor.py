740f84ed086f7d368656a01b93cdabad71c83a4b
Annadaliah <112446326+Annadaliah@users.noreply.github.com>
2024-09-18 07:24:11 -0400

    completed lab 3

--- labs/lab-3/db/schema/professor.py
+++ labs/lab-3/db/schema/professor.py
@@ -9,12 +9,18 @@ class Professor(db.Model):
     course = db.relationship('Courses', secondary = 'ProfessorCourse', back_populates = 'Professors')
     def __init__(self):
         # remove pass and then initialize attributes
-        pass
+        self.ProfessorID = self.ProfessorID
+        self.ProfessorFirstName = self.ProfessorFirstName
+        self.ProfessorLastName = self.ProfessorLastName
+        self.ProfessorEmail = self.ProfessorEmail
 
     def __repr__(self):
         # add text to the f-string
         return f"""
-
+            "PROFESSOR ID: {self.ProfessorID}
+             PROFESSOR FIRST NAME: {self.ProfessorFirstName}
+             PROFESSOR LAST NAME: {self.ProfessorLastName}
+             PROFESSOR EMAIL: {self.ProfessorEmail}
         """
     
     def __repr__(self):
