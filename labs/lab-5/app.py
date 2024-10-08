55294f623729f2642f2d9af3788af20781b4b30d
Annadaliah <112446326+Annadaliah@users.noreply.github.com>
2024-10-08 16:20:58 -0400

    lab 5 done

--- labs/lab-5/app.py
+++ labs/lab-5/app.py
@@ -1,12 +1,24 @@
 """app.py: render and route to webpages"""
-from flask import render_template
+from flask import Flask, render_template
 
 from db.server import app
+
+from socketserver import *
+
+app = Flask(__name__)
 
 # create a webpage based off of the html in templates/index.html
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/templates/')
+def homepage():
+    return render_template("homepage.html")
+
+@app.route('/templates/')
+def stuff():
+    return render_template("stuff.html")
 
 if __name__ == "__main__":
     # debug refreshes your application with your new changes every time you save
