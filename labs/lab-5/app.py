b6e939f70b948f0bc2c2bbd70b69c1ea72acae0a
Annadaliah <112446326+Annadaliah@users.noreply.github.com>
2024-10-15 13:37:14 -0400

    lab 5

--- labs/lab-5/app.py
+++ labs/lab-5/app.py
@@ -1,5 +1,5 @@
 """app.py: render and route to webpages"""
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 from db.server import app
 
@@ -10,9 +10,17 @@ app = Flask(__name__)
 # create a webpage based off of the html in templates/index.html
 @app.route('/')
 def index():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
     return render_template("index.html")
 
-@app.route('/templates/')
+@app.route('/templates/', methods=['GET', 'POST'])
 def homepage():
     return render_template("homepage.html")
 
